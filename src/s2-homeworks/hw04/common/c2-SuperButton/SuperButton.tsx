6e4e6e4c002206dcbae7160431d2e18c19fc3b70
AnnaTsishk <05072022novat@gmail.com>
2022-11-11 22:15:26 +0300

    Commit-1

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,12 +1,14 @@
 import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
 import s from './SuperButton.module.css'
 
+
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
 type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement>
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
     xType?: string
+
 }
 
 const SuperButton: React.FC<SuperButtonPropsType> = (
@@ -18,11 +20,20 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        + (disabled
+                ? ' ' + s.disabled
+                : xType === 'red'
+                    ? ' ' + s.red
+                    : xType === 'secondary'
+                        ? ' ' + s.secondary
+                        : ' ' + s.default
+        )
+
+
+        + (className ? '' + className : '')
+
+
+    // задачка на смешивание классов
 
     return (
         <button
