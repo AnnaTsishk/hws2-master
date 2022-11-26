013426f0af585c8947c3296dfd4f9afd6e2b225c
AnnaTsishk <05072022novat@gmail.com>
2022-11-26 12:35:23 +0300

    HW1,HW2, HW3, HW4, HW5

---
+++ src/s2-homeworks/h5-rrd-v6/Pages.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Navigate, Route, Routes} from "react-router-dom";
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
+import JuniorPlus from "../hw05/pages/JuniorPlus";
+import Junior from "../hw05/pages/Junior";
+
+export const PATH = {
+    PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/pre-junior',
+    JUNIOR_PLUS: '/pre-junior'
+    // add paths
+}
+
+function Pages() {
+    return (
+        <div>
+            {/*Routes выбирает первый подходящий роут*/}
+            <Routes>
+
+                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+                // add routes
+
+                {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+                <Route path={'/*'} element={<Error404/>}/>
+
+            </Routes>
+        </div>
+    )
+}
+
+export default Pages
